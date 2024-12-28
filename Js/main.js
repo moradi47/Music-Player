30893376299c27a849eacf4258b20fd56eaffe29
ms.moradi <ms.moradi47@gmail.com>
2024-12-28 19:28:26 +0330

    Definition of a presentation that includes a music list

---
+++ Js/main.js
@@ -0,0 +1,27 @@
+const musics = [
+    {
+        name: 'Approaching Dusk',
+        src:'',
+        poster:'',
+    },
+    {
+        name: 'Beau',
+        src:'',
+        poster:'',
+    },
+    {
+        name: 'Emerald and Stone',
+        src:'',
+        poster:'',
+    },
+    {
+        name:'Remembering',
+        src:'',
+        poster:'',
+    },
+    {
+        name:' The World Is Turning',
+        src:'',
+        poster:'',
+    }
+]
