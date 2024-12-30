bb63a3e3a7b7690b7f0622c9902745bb6e12d787
ms.moradi <ms.moradi47@gmail.com>
2024-12-30 16:42:53 +0330

    Editing music information and writing a function for each element of the music name

--- Js/main.js
+++ Js/main.js
@@ -1,27 +1,55 @@
 const musics = [
     {
         name: 'Approaching Dusk',
-        src:'',
-        poster:'',
+        src:'../audio/approaching-dusk.mp3',
+        poster:'../image/image1.jpg',
+        artist:'',
     },
     {
         name: 'Beau',
-        src:'',
-        poster:'',
+        src:'../audio/beau.mp3',
+        poster:'../image/image2.jpg',
+        artist:'',
     },
     {
         name: 'Emerald and Stone',
-        src:'',
-        poster:'',
+        src:'../audio/emerald-and-stone.mp3',
+        poster:'../image/image3.jpg',
+        artist:'',
     },
     {
         name:'Remembering',
-        src:'',
-        poster:'',
+        src:'../audio/remembering.mp3',
+        poster:'../image/image4.jpg',
+        artist:'',
     },
     {
-        name:' The World Is Turning',
-        src:'',
-        poster:'',
+        name:'The World Is Turning',
+        src: '../audio/the-world-is-turning.mp3',
+        poster:'../image/image5.jpg',
+        artist:'',
     }
 ]
+
+let liElem = document.querySelectorAll('.music');
+let imageElem = document.querySelector('.poster-image');
+let audioElem = document.querySelector('.audio');
+let artistName = document.querySelector('.artist');
+let nameMusic = document.querySelector('.name-music');
+
+console.log(artistName);
+
+liElem.forEach((li)=>{
+    li.addEventListener('click', function(event){
+
+        musics.forEach((music)=>{
+            
+            if(event.target.innerHTML === music.name){
+                imageElem.src = music.poster;
+                audioElem.src = music.src;
+                nameMusic.innerHTML = music.name;
+                artistName.innerHTML = music.artist;
+            }
+        })
+    })
+})
